76c4a76c352a1ac36e35a32b995328fe60c7019b
alimtorofder1 <alimtorofder43@gmail.com>
2024-12-03 14:59:14 +0600

    add

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+const cookieParser = require("cookie-parser");
 
 app.use(cors());
 app.use(express.json());
-
+app.use(cookieParser());
 
 
 
